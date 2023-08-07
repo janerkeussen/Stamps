861156663bc8aa193cf9c65b6ef706eb0ce4643c
Janerke <zhanerke.usen@gmail.com>
2023-08-07 08:52:03 -0700

    add stamp where taps occur

--- Stamps/ViewController.swift
+++ Stamps/ViewController.swift
@@ -9,16 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet private var buttons: [UIButton]!
-    private var selectedColor: UIColor = .black
-
+    private var selectedColor: UIColor = .white
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(tapped))
+        view.addGestureRecognizer(tap)
     }
-
+    
+    @objc private func tapped(_ tap: UITapGestureRecognizer) {
+        let loc = tap.location(in: self.view)
+        addStamp(at: loc)
+    }
+    
+    private func addStamp(at loc: CGPoint) {
+        let stamp = UIView(frame: CGRect(x: loc.x, y: loc.y, width: 50, height: 50))
+        stamp.backgroundColor = selectedColor
+        view.addSubview(stamp)
+    }
+    
     @IBAction private func buttonTapped(_ sender: UIButton) {
-       guard
-        let selectedColor = sender.backgroundColor else { return }
+        guard
+            let selectedColor = sender.backgroundColor else { return }
         self.selectedColor = selectedColor
         updateButton(sender)
     }
@@ -27,6 +39,6 @@ class ViewController: UIViewController {
         buttons.forEach {  $0.alpha = 1 }
         button.alpha = 0.8
     }
-
+    
 }
 
