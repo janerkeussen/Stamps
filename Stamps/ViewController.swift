93e680591b5c0704f4655ea9d66462e981cb3e7a
Janerke <zhanerke.usen@gmail.com>
2023-08-06 23:27:27 -0700

    initial app setup - add buttons, add button taps

--- Stamps/ViewController.swift
+++ Stamps/ViewController.swift
@@ -8,12 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet private var buttons: [UIButton]!
+    private var selectedColor: UIColor = .black
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
+    @IBAction private func buttonTapped(_ sender: UIButton) {
+       guard
+        let selectedColor = sender.backgroundColor else { return }
+        self.selectedColor = selectedColor
+        updateButton(sender)
+    }
+    
+    private func updateButton(_ button: UIButton) {
+        buttons.forEach {  $0.alpha = 1 }
+        button.alpha = 0.8
+    }
 
 }
 
